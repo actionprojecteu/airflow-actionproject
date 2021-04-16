f2464222f9cdc0eba77a4fb73079428ca699af24
Rafael González <astrorafael@gmail.com>
2021-04-16 10:27:03 +0200

    added two new fields to the Zooniverse export table

--- src/airflow_actionproject/operators/data/streetspectra.sql
+++ src/airflow_actionproject/operators/data/streetspectra.sql
@@ -1,19 +1,21 @@
 -----------------------------------------------------------------------------
--- Auxiliar database to avoid dupliacte observations into the ACTION Database
+-- Auxiliar database to avoid duplicate observations into the ACTION Database
 -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS zooniverse_t
+CREATE TABLE IF NOT EXISTS zooniverse_export_t
 (
 	classification_id	INTEGER,
+	user_name           TEXT, 	-- only for registered users
+	user_id             TEXT,	-- only for registered users
 	workflow_id			INTEGER,	
 	workflow_name		TEXT,	
 	workflow_version	TEXT,
 	created_at			TEXT,	
 	gold_standard		TEXT,	
 	expert				TEXT,	
-	metadata			TEXT,	-- long JSON string with netsted info
-	annotations			TEXT,	-- long JSON string with netsted info
-	subject_data		TEXT,	-- long JSON string with netsted info
+	metadata			TEXT,	-- long JSON string with nested info
+	annotations			TEXT,	-- long JSON string with nested info
+	subject_data		TEXT,	-- long JSON string with nested info
 	subject_ids			TEXT,	-- JSON string
 
 	PRIMARY KEY(classification_id)
