5bba8375ca0161a7567f766b1f8f693721bbce44
Rafael González <astrorafael@gmail.com>
2021-04-19 14:03:24 +0200

    varoiable setter operator

--- src/airflow_actionproject/operators/action.py
+++ src/airflow_actionproject/operators/action.py
@@ -17,7 +17,7 @@ import json
 # Airflow imports
 # ---------------
 
-from airflow.models import BaseOperator
+from airflow.models import BaseOperator, Variable
 from airflow.utils.decorators import apply_defaults
 
 #--------------
@@ -162,6 +162,71 @@ class ActionDownloadFromStartDateOperator(BaseOperator):
 				self.log.info(f"Got {N} entries, excess of {excess} entries found")
 				observations = observations[:-excess]
 			self.log.info(f"Fetched {len(observations)} entries")
+		# Make sure the output directory exists.
+		output_dir = os.path.dirname(self._output_path)
+		os.makedirs(output_dir, exist_ok=True)
+		with open(self._output_path, "w") as fd:
+			json.dump(observations, indent="",fp=fd)
+			self.log.info(f"Written entries to {self._output_path}")
+
+
+class ActionDownloadFromVariableDateOperator(BaseOperator):
+	"""
+	Operator that fetches N observations from the ACTION database
+	starting from a date specified by an Airflow Variable name.
+	After fetching that many observations, it updates the variable
+	with the timestamp of the next observation pending to read.
+	Parameters
+	—————
+	conn_id : str
+	ID of the connection to use to connect to the Epicollect V API. 
+	output_path : str
+	Path to write the fetched entries to.
+	variable_name : str
+	Name of the Airflow Variable that contaoins the start timestamp.
+	Expected format is YYYY-MM-DD (equal to Airflow"s ds formats).
+	end_date : str
+	(Templated) end date to fetching entries up to (exclusive).
+	Expected format is YYYY-MM-DD (equal to Airflow"s ds formats).
+	"""
+	
+	
+	template_fields = ("_output_path",)
+
+	@apply_defaults
+	def __init__(self, conn_id, output_path, variable_name,  project, n_entries, obs_type='observation', **kwargs):
+		super().__init__(**kwargs)
+		self._conn_id = conn_id
+		self._output_path = output_path
+		self._key = variable_name
+		self._end_date = '2999-12-31'	# far away
+		self._n_entries = n_entries + 1
+		self._obs_type = obs_type
+		self._project = project
+
+
+	def execute(self, context):
+		start_date = Variable.get(self._key)
+		with ActionDatabaseHook(self._conn_id) as hook:
+			self.log.info(f"Fetching {self._n_entries} entries from date {self._start_date}")
+			observations = list(
+				hook.download( 
+					start_date = start_date,
+					end_date   = self._end_date,
+					n_entries  = self._n_entries,
+					project    = self._project,
+					obs_type   = self._obs_type,
+				)
+			)
+			N = len(observations)
+			excess = N - self._n_entries
+			if excess > 0:
+				self.log.info(f"Got {N} entries, excess of {excess} entries found")
+				observations = observations[:-excess]
+			self.log.info(f"Fetched {len(observations)} entries")
+		# Removes the last item and updates the timestamp marker
+		last_item = observations.pop()
+		Variable.set(self._key, last_item['created_at'])
 		# Make sure the output directory exists.
 		output_dir = os.path.dirname(self._output_path)
 		os.makedirs(output_dir, exist_ok=True)
