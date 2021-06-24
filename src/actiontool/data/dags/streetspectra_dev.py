6dec17560cf84101c40a863b8f67fb63f063f428
Rafael González <astrorafael@gmail.com>
2021-06-24 13:19:52 +0200

    changed dags

--- src/actiontool/data/dags/streetspectra_dev.py
+++ src/actiontool/data/dags/streetspectra_dev.py
@@ -46,8 +46,8 @@ from airflow_actionproject.callables.action        import check_number_of_entrie
 default_args = {
     'owner'           : 'airflow',
     'depends_on_past' : False,
-    'email'           : ("astrorafael@gmail.com",),
-    'email_on_failure': False,
+    'email'           : ("astrorafael@gmail.com",), # CAMBIAR AL VERDADERO EN PRODUCCION
+    'email_on_failure': False,                      # CAMBIAR A True EN PRODUCCION
     'email_on_retry'  : False,
     'retries'         : 1,
     'retry_delay'     : timedelta(minutes=5),
