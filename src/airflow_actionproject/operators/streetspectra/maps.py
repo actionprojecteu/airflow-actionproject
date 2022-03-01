0de2befab481d64f27f8d98563226b8e9feecc70
Rafael González <astrorafael@gmail.com>
2022-03-01 23:09:22 +0100

    added metadata link

--- src/airflow_actionproject/operators/streetspectra/maps.py
+++ src/airflow_actionproject/operators/streetspectra/maps.py
@@ -141,10 +141,11 @@ class FoliumMapOperator(BaseOperator):
     template_fields = ("_input_path", "_output_path")
 
     @apply_defaults
-    def __init__(self, input_path, output_path, ssh_conn_id, remote_slug, center_latitude, center_longitude, zoom_start=6, max_zoom=19, **kwargs):
+    def __init__(self, input_path, output_path, ssh_conn_id, imag_remote_slug, meta_remote_slug, center_latitude, center_longitude, zoom_start=6, max_zoom=19, **kwargs):
         super().__init__(**kwargs)
         self._ssh_conn_id = ssh_conn_id
-        self._remote_slug = remote_slug
+        self._img_remote_slug = imag_remote_slug
+        self._meta_remote_slug = meta_remote_slug
         self._input_path = input_path
         self._output_path = output_path
         self._context = {}
@@ -185,7 +186,8 @@ class FoliumMapOperator(BaseOperator):
         observer = "anonymous" if observer == '' else observer
         item['observer'] = strip_email(observer)
         # Get the name parameter of URL
-        item['new_url'] = self._urlbase + item['url'].split('name=')[-1]
+        item['new_url']  = self._urlbase + item['url'].split('name=')[-1]
+        item['metadata_url'] = self._urlbase2 + item['id'] + '.json'
         return item
 
     def _good_coordinates(self, item):
@@ -207,7 +209,8 @@ class FoliumMapOperator(BaseOperator):
         )
         with SCPHook(self._ssh_conn_id) as hook:
             host, _, _, _,_ = hook.get_conn()
-            self._urlbase = f"https://{host}/{self._remote_slug}/"
+            self._urlbase  = f"https://{host}/{self._img_remote_slug}/"
+            self._urlbase2 = f"https://{host}/{self._meta_remote_slug}/"
         with open(self._input_path) as fd:
             observations = json.load(fd)
             self.log.info(f"Parsed {len(observations)} observations from {self._input_path}")
